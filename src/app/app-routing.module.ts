6692a40ecdb9626541c48940c12d0bc0d8ff214b
Diego187 <dt18702@gmail.com>
2022-04-13 21:55:55 -0600

    Login y plantiila de creación de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 const routes: Routes = [
 
 {path:'login',component:LoginComponent},
-{path:'home',component:HomeComponent}
+{path:'home',component:HomeComponent},
+{path: 'createUser',component:CreateUserComponent}
 
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +13,14 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
 
-    
+  formularioLogin(){
 
+  }
 
-
-
+  createUser(){
+    alert("prueba")
   }
 
 }
