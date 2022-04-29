ff6d29f19a123234e9108f40c96303489eb0daf9
ricardochavz <ricardo.taracena64@gmail.com>
2022-04-28 23:07:28 -0600

    finalizacion diseno login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-
+  mensaje: String = "prueba";
+  prueba: string = "Ingrese un nombre";
+  mostrar: boolean =false;
+  user: any= {};
+  usuarioInvalido: boolean=false;
+  
+  
+ constructor(private http: HttpClient) { }
     
-
-
-
-
+  ngOnInit(): void {
   }
+
+ mostrarMensaje(){
+    this.mostrar = !this.mostrar;
+  }  
+
+    formulariologin(){
+      let formularioValido : any = document.getElementById("loginform");
+      if(formularioValido.reportValidity()){
+        
+        this.servicioLogin().subscribe(
+          (respuesta:any)=> this.login(respuesta)
+        )
+      }
+    }
+  
+    login(res:any){
+                
+      if(res==null){
+        this.usuarioInvalido=true;
+      }
+      else if(res=="e"){
+        alert("trono")
+      }
+      else if(res!=null){
+        localStorage.setItem("user",JSON.stringify(res));
+        location.href="/login"; 
+      }
+      console.log(res);
+  
+  
+    }
+  
+    servicioLogin(){
+      var httpOptions={
+        headers:new HttpHeaders({ 
+          'Content-Type':'application/json'
+        })
+      }
+  
+      return this.http.post<any>("http://localhost:3606/login/user", this.user, httpOptions).pipe(
+        catchError(e=>"e")
+      )
+  
+    }
+
+
+
+  
 
 }
