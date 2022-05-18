5f6df19e80b90a5fc397cbcd73ab1e1e0a3762a0
fernandojosecc <fernandojosecc@hotmail.com>
2022-05-17 20:49:06 -0700

    Arreglando error de la ruta de User-Edition

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { UserCreationComponent } from './user-creation/user-creation.component';
 import { ChatComponent } from './chat/chat.component';
+import { UserEditionComponent } from './user-edition/user-edition.component';
 
 const routes: Routes = [
 
 {path:'',component:LoginComponent},  
 {path:'home',component:HomeComponent},
 {path:'user-creation',component:UserCreationComponent},
+{path:'user-edition',component:UserEditionComponent},
 {path: 'chat', component:ChatComponent}
 
 
