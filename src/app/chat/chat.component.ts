45ae086dd3808322ec64c727742661baf26d9f63
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-05-20 23:27:04 -0600

    cambios de front

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { mergeAll } from 'rxjs/operators';
+import {catchError, find, mergeAll} from 'rxjs/operators';
+import {BehaviorSubject} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 
 @Component({
@@ -15,20 +17,34 @@ export class ChatComponent implements OnInit {
   newMessages:any = {}
   SMS = ""
   newSMS:any={}
-  
+  chats:any = {}
 
-  constructor() { }
+
+    message = new BehaviorSubject("valor incial")
+    mensaje = 'inicio de mensaje';
+    constructor(private http: HttpClient) {
+      this.message.subscribe(data => {
+        console.log('recibiendo.... ' + data)
+        this.mensaje = this.mensaje+' | '+ data
+
+      })
+  }
+
+
 
   ngOnInit(): void {
     this.user = localStorage.getItem("user");
     this.user = JSON.parse(this.user);
     this.nameUser = this.user[0].user;
-
     this.chat = localStorage.getItem("chat")
     this.chat = JSON.parse(this.chat)
     this.cargarChats();
-
   }
+
+  public agregaT(texto:string):void{
+    this.message.next(texto)
+  }
+
 
   cargarChats(){
    this.messages = this.chat.messages
@@ -37,26 +53,43 @@ export class ChatComponent implements OnInit {
   sendSMS(){
     var date = new Date()
     var month = date.getMonth() + 1
-    var newSMS = "{date: '" + date.getDate() + "/" + month 
-    + "/" + date.getFullYear() + "', time: '" + date.getHours() 
-    + ":" + date.getMinutes() + "', user: '" + this.nameUser 
+    var newSMS = "{date: '" + date.getDate() + "/" + month
+    + "/" + date.getFullYear() + "', time: '" + date.getHours()
+    + ":" + date.getMinutes() + "', user: '" + this.nameUser
     + "', message: '" + this.SMS + "'}"
-    
     this.newMessages = this.messages
-    
     this.newMessages.push(newSMS)
-
-    
     console.log(this.newMessages)
-
-
-   
   }
 
+    findChats(){
+      var httpOptions={
+        headers:new HttpHeaders({
+          'Content-Type':'application/json'
+        })
+      }
+      return this.http.get<any>("http://localhost:4042/chat/find", httpOptions).pipe(
+        catchError(e=>"E")
+      )
+    }
+
+
+    formatChat(res:any){
+      this.chats = JSON.stringify(res)
+      this.chats = JSON.parse(this.chats)
+      console.log(this.chats)
+    }
+
+    find(){
+      this.findChats().subscribe(
+        (response:any)=>this.formatChat(response)
+      )
+    }
 
 
   signOff(){
     localStorage.clear()
     location.href="#"
   }
-}
+
+  }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import {catchError} from 'rxjs/operators';
+import {catchError, find} from 'rxjs/operators';
 import { HttpHeaders } from '@angular/common/http';
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -11,26 +12,22 @@ import { HttpHeaders } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   user:any={};
   nameUser = ""
-  chats:any = {}
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.user = localStorage.getItem("user");
-    this.user = JSON.parse(this.user);
-    this.nameUser = this.user[0].user;
-    
-    console.log(this.user)
-    console.log(this.user[0].user);
-    
-
-    this.findChats().subscribe(
-      (respuesta:any) => this.formatChat(respuesta)
-    )
-    
+  constructor(private http: HttpClient) {
   }
 
-  getChat(name:any){
+  ngOnInit(): void {
+    /*this.find()*/
+    this.user = localStorage.getItem("user")
+    this.user = JSON.parse(this.user)
+    this.nameUser = this.user[0].user
+
+   /* this.findChats().subscribe(
+      (respuesta:any) => this.formatChat(respuesta)
+    )*/
+  }
+
+/*  getChat(name:any){
     console.log(name)
     for(let chat of this.chats){
       if(chat.name == name){
@@ -38,25 +35,6 @@ export class HomeComponent implements OnInit {
         location.href="/chat"
       }
     }
-    
-  }
-
-
-  formatChat(res:any){
-    this.chats = JSON.stringify(res)
-    this.chats = JSON.parse(this.chats)
-    console.log(this.chats)
-  }
-
-  findChats(){
-    var httpOptions={
-      headers:new HttpHeaders({
-        'Content-Type':'application/json'
-      })
-    }
-    return this.http.get<any>("http://localhost:4042/chat/find", httpOptions).pipe(
-      catchError(e=>"E")
-    )
-  }
+  }*/
 
 }
